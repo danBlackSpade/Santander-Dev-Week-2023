c596732a4b38dab97f4c10c9cb42b86d3691d25b
Daniel J <djun44@gmail.com>
2023-08-20 10:33:13 -0300

    add update user

--- script.py
+++ script.py
@@ -9,12 +9,14 @@ df = pd.read_csv('SDW2023.csv')
 user_ids = df['UserID'].tolist()
 print(user_ids)
 
+
 def get_user(id):
   response = requests.get(f'{sdw2023_api_url}/users/{id}')
   return response.json() if response.status_code == 200 else None
 
 users = [user for id in user_ids if (user := get_user(id)) is not None]
 
+print(users)
 
 df_messages = pd.read_csv('MensagensMKT.csv', delimiter=";")
 msgs = df_messages["Mensagem"].tolist()
@@ -27,7 +29,15 @@ for user in users:
   print(user["id"])
   msg = generate_news(user)
   user['news'].append({
-    "icon": "https://digitalinnovationone.github.io/santander-dev-week-2023-api/icons/credit.svg",
-    "description2": f"{user['name']}, {msg}",
+    "icon": "🤑",
+    "description": f"{user['name']}, {msg}",
   })
   print(json.dumps(user, indent=2))
+
+def update_user(user):
+  response = requests.put(f"{sdw2023_api_url}/users/{user['id']}", json=user)
+  return True if response.status_code == 200 else False
+
+for user in users:
+  success = update_user(user)
+  print(f"User {user['name']} updated? {success}!")
